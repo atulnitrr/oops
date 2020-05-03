a76cf5c1b8055f7e2407ee37451e659cda235e17
Atul Kumar <atul.kumar3@go-mmt.com>
2020-05-04 04:32:58 +0530

    Initial commit

---
+++ src/test/java/com/atul/oops/oops/OopsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.atul.oops.oops;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OopsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
